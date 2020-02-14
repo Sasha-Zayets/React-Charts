1ee979458085b2d4542d238316d32c26b55c85cf
Sasha-Zayets <sachazaets@ukr.net>
2020-02-15 01:06:56 +0200

    Updates

---
+++ precache-manifest.3988663efcaea46c63ce3cfe160625d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21790e5bbc3e754284a38e9bd5673c1f",
+    "url": "/React-Charts/index.html"
+  },
+  {
+    "revision": "bc4c51d486202cf33be1",
+    "url": "/React-Charts/static/css/main.2d535cbf.chunk.css"
+  },
+  {
+    "revision": "b16f974cf9616ecb6179",
+    "url": "/React-Charts/static/js/2.f7763db9.chunk.js"
+  },
+  {
+    "revision": "10c3c4f73b0578a837b96ef723e22acf",
+    "url": "/React-Charts/static/js/2.f7763db9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc4c51d486202cf33be1",
+    "url": "/React-Charts/static/js/main.46459a31.chunk.js"
+  },
+  {
+    "revision": "09f4f1e38bc3239976b8",
+    "url": "/React-Charts/static/js/runtime-main.e2254437.js"
+  }
+]);
